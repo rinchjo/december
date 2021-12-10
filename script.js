1fce09b6da7be5819a1418c22f3f7ef769364a0a
rinchjo <83864991+rinchjo@users.noreply.github.com>
2021-12-10 16:34:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,40 @@
-// Regular Expressions
+const fullname = document.querySelector("#fullname");
+const email = document.querySelector("#email");
+const number = document.querySelector("#number");
+const password = document.querySelector("#password");
+const register = document.querySelector("#register");
+const image = document.querySelector("#image");
 
-// Primitive DataTypes - Number , String , Boolean , null , undefined
-// Non-Primitive DataTypes - Object , Arrays , Regular Expressions
-// {} - Object
-// [] - Arrays
-// // - RegExp
+var users;
 
-// | - or logic
-// g - taarch bgaa buh ogogdliig tsugluulj avna
-// i - ignore the case - tom jijig usgiin ylgaag arilgana
-// . - tsegiin oron ymarch temdegt orj ireh bolomjtoi gj uzne
-// ^ (carret) - Өгөгдөл ямар тэмдэгтээр (үг) эхлэхийг заана
-// $ - Өгөгдөл ямар тэмдэгтээр (үг) төгсөхийг заана
-// + - dor hayj ng udaa taarah case
-// * - 0 bolon tuunees ih niitsel
-// [] - herev haaltan dotor ^ orj irvel haaltan dotorh temdegtees busad buh ogogdliig avna
-// \d == [0-9] - бүх тоон өгөгдөл
-// \D == [^0-9] - тооноос бусад бүх өгөгдөл
-// \s == ' ' - white space
-// \S == [^ ] - white space-с бусад бүх өгөгдөл
-// \w == [a-zA-Z0-9_] - бүх том жижиг үсэг, тоонууд болон доогуур зураас
-// \W == [^a-zA-Z0-9_] - Онцгой тэмдэгтүүд
-// {n,} - хамгийн багадаа таарах өгөгдөл
-// {n,m} - өгөгдөл таарах завсрыг илэрхийлнэ
-// {,n} - хамгийн ихдээ таарах өгөгдөл
-// {n}
+if (localStorage["users"]) {
+  users = JSON.parse(localStorage["users"]);
+} else {
+  users = [];
+}
 
-// const tomUsegAguuljBn = /[A-Z]/;
+class User {
+  constructor(fullname, email, number, password, avatar) {
+    this.fullname = fullname;
+    this.email = email;
+    this.number = number;
+    this.password = password;
+    this.avatar = avatar;
+    this.id = Math.random().toString().split(".")[1];
+  }
+}
 
-// const password = "myPass1";
+register.addEventListener("click", function (e) {
+  e.preventDefault();
 
-// console.log(tomUsegAguuljBn.test(password));.
+  const user = new User(
+    fullname.value,
+    email.value,
+    number.value,
+    password.value,
+    image.value
+  );
 
-const input = document.querySelector("#input");
-const texts = document.querySelectorAll(".text");
-
-input.addEventListener("input", () => {
-  const regExp = [/[a-z]/, /[A-Z]/, /[0-9]/, /\W/, /.{8,}/];
-  regExp.forEach((exp, index) => {
-    if (exp.test(input.value)) {
-      texts[index].classList.remove("text-red-400");
-      texts[index].classList.add("text-gray-400");
-      texts[index].classList.add("line-through");
-    } else {
-      texts[index].classList.add("text-red-400");
-      texts[index].classList.remove("text-gray-400");
-      texts[index].classList.remove("line-through");
-    }
-  });
-
-  const checkPass = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\W)(?=.{8,})/;
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
 });
-
-// Password strength shalgah function bich
-// 1. Tom useg aguulsan bh
-// 2. Jijig useg aguulsan bh
-// 3. Too aguulsan bh
-// 4. Temdegt aguulsan bh
-// 5. hamgiin bagadaa 8 orontoi bh
-
-// Gert hex shalgah rexExp bichih - #AAABBB - ^ ... $
-//  {3, 6}$
-// 3 element
-// 6 element
-
-// a-f - 0-9
